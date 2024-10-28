983b4b5bba13bfa864a6a34567fe1cc2b0885786
selenagomez25 <92699085+selenagomez25@users.noreply.github.com>
2024-10-27 21:27:49 -0500

    update to latest

--- files/memory.hpp
+++ files/memory.hpp
@@ -7,92 +7,81 @@
 
 #include <Windows.h>
 #include <TlHelp32.h>
-
 #include <cstdint>
 #include <string_view>
-#include <functional>
 #include <memory>
 
 class Memory
 {
 private:
     std::uintptr_t procId = 0;
-    HANDLE processHandle = nullptr;
-
-    auto closeHandle = [](HANDLE handle) {
-        if (handle) ::CloseHandle(handle);
-    };
 
 public:
-    Memory(const std::string_view processName) noexcept
+    HANDLE processHandle = nullptr;
+
+    Memory(const std::wstring_view processName) noexcept
     {
-        ::PROCESSENTRY32 entry = {};
-        entry.dwSize = sizeof(::PROCESSENTRY32);
+        PROCESSENTRY32W entry = { sizeof(PROCESSENTRY32W) };
+        const auto procSnap = std::unique_ptr<void, decltype(&CloseHandle)>(
+            CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0), CloseHandle);
 
-        const auto procSnap = std::unique_ptr<void, decltype(closeHandle)>(
-            ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0), closeHandle);
-
-        auto findProcessId = [&](auto&& name) -> std::uintptr_t {
-            while (::Process32Next(procSnap.get(), &entry)) {
-                if (!name.compare(entry.szExeFile)) {
-                    return entry.th32ProcessID;
+        if (Process32FirstW(procSnap.get(), &entry)) {
+            do {
+                if (processName == entry.szExeFile) {
+                    procId = entry.th32ProcessID;
+                    break;
                 }
-            }
-            return 0;
-        };
+            } while (Process32NextW(procSnap.get(), &entry));
+        }
 
-        procId = findProcessId(processName);
-
-        processHandle = ::OpenProcess(PROCESS_ALL_ACCESS, FALSE, procId);
+        if (procId)
+            processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procId);
     }
 
     ~Memory()
     {
-        closeHandle(processHandle);
+        if (processHandle)
+            CloseHandle(processHandle);
     }
 
-    const std::uintptr_t GetModuleAddress(const std::string_view moduleName) const noexcept
+    const std::uintptr_t GetModuleAddress(const std::wstring_view moduleName) const noexcept
     {
-        ::MODULEENTRY32 entry = {};
-        entry.dwSize = sizeof(::MODULEENTRY32);
+        MODULEENTRY32W entry = { sizeof(MODULEENTRY32W) };
+        const auto modSnap = std::unique_ptr<void, decltype(&CloseHandle)>(
+            CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId), CloseHandle);
 
-        const auto procSnap = std::unique_ptr<void, decltype(closeHandle)>(
-            ::CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId), closeHandle);
-
-        auto findModuleBase = [&](auto&& name) -> std::uintptr_t {
-            while (::Module32Next(procSnap.get(), &entry)) {
-                if (!name.compare(entry.szModule)) {
+        if (Module32FirstW(modSnap.get(), &entry)) {
+            do {
+                if (moduleName == entry.szModule) {
                     return reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
                 }
-            }
-            return 0;
-        };
-
-        return findModuleBase(moduleName);
+            } while (Module32NextW(modSnap.get(), &entry));
+        }
+        return 0;
     }
 
     template <typename T>
     constexpr const T Read(const std::uintptr_t& address) const noexcept
     {
         T value = {};
-        ::ReadProcessMemory(processHandle, reinterpret_cast<const void*>(address), &value, sizeof(T), NULL);
+        ReadProcessMemory(processHandle, reinterpret_cast<const void*>(address), &value, sizeof(T), NULL);
         return value;
     }
 
     template <typename T>
     constexpr void Write(const std::uintptr_t& address, const T& value) const noexcept
     {
-        ::WriteProcessMemory(processHandle, reinterpret_cast<void*>(address), &value, sizeof(T), NULL);
+        WriteProcessMemory(processHandle, reinterpret_cast<void*>(address), &value, sizeof(T), NULL);
     }
 
     LPVOID AllocateMemory(SIZE_T size, DWORD allocationType = MEM_COMMIT | MEM_RESERVE, DWORD protect = PAGE_READWRITE) const noexcept
     {
-        return ::VirtualAllocEx(processHandle, nullptr, size, allocationType, protect);
+        return VirtualAllocEx(processHandle, nullptr, size, allocationType, protect);
     }
 
     BOOL FreeMemory(LPVOID address, SIZE_T size, DWORD freeType = MEM_RELEASE) const noexcept
     {
-        return ::VirtualFreeEx(processHandle, address, size, freeType);
+        return VirtualFreeEx(processHandle, address, size, freeType);
     }
 };
 
